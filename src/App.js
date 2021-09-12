e6100ef0cddb7462d9c2ede8f050d2be8e645f50
catoprog <@DESKTOP-GTV0AGT>
2021-09-12 15:32:06 +0300

    hw3 done

--- src/App.js
+++ src/App.js
@@ -5,35 +5,45 @@ import { POKEMONS } from './Pokemons';
 import PokemonCard from './components/PokemonCard';
 import Bg1Img from './image/bg1.jpg';
 import Bg3Img from './image/bg3.jpg';
+import s from './App.css'
 
 const App = () => {
   return (
     <>
-      <Header 
+      <Header
         title="This is new Title!"
         hideBackgraund
       />
       <Layout
         urlBg={Bg1Img}
-        title= "Hi!"
-        descr= "This is the first layout!"
-      />
+        title="Hi!"
+      >
+        <p>In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3 grid.
+          Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them into the player's own color of red or blue.</p>
+        <p>To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the player's color instead.</p>
+      </Layout>
       <Layout
         colorBg="antiquewhite"
-        descr="This is the second layout!"
       >
-        {/* <div className ="flex">
+        <div className="flex">
           {
-            POKEMONS.map(item => <PokemonCard {...item}/>)
+            POKEMONS.map(item => <PokemonCard
+              key={item.id}
+              name={item.name}
+              img={item.img}
+              id={item.id}
+              type={item.type}
+              values={item.values}
+            />)
           }
-        </div> */}
+        </div>
       </Layout>
 
       <Layout
         urlBg={Bg3Img}
-        descr="This is the third layout!"
+        title="Bye!"
       />
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,32 +1,42 @@
+import s from './style.module.css'
 
-const PokemonCard = ({name, img, id, type, values}) => {
+import cardBackSide from '../../image/card-back-side.jpg'
+import { useState } from 'react'
+
+const PokemonCard = ({ name, img, id, type, values }) => {
+    const [isActive, setActive] = useState(false);
+    const handleClick = () => {
+        setActive(!isActive);
+    }
     return (
-        <div className="root">
-            <div className="pokemonCard">
-                <div className="cardFront">
-                    <div className="wrap front">
-                        <div className="pokemon <-- Type Pokemon -->">
-                            <div className="values">
-                                <div className="count top">{values.top}</div>
-                                <div className="count right">{values.right}</div>
-                                <div className="count bottom">{values.bottom}</div>
-                                <div className="count left">{values.left}</div>
+        <div className={s.root} onClick={handleClick}>
+            <div className={`${s.pokemonCard} ${isActive ? s.active : ''}`}>
+                <div className={s.cardFront}>
+                    <div className={`${s.wrap} ${s.front}`}>
+                        <div className={`${s.pokemon} ${s[type]}`}>
+                            <div className={s.values}>
+                                <div className={`${s.count} ${s.top}`}>{values.top}</div>
+                                <div className={`${s.count} ${s.right}`}>{values.right}</div>
+                                <div className={`${s.count} ${s.bottom}`}>{values.bottom}</div>
+                                <div className={`${s.count} ${s.left}`}>{values.left}</div>
                             </div>
-                            <div className="imgContainer">
-                                <img src="<-- Pokemon Picture -->" alt="<-- Name Pokemon -->" />
+                            <div className={s.imgContainer}>
+                                <img src={img} alt={name} />
                             </div>
-                            <div className="info">
-                                <span className="number">#{id}</span>
-                                <h3 className="name">{name}</h3>
-                                <small className="type">Type: <span>{type}</span></small>
+                            <div className={s.info}>
+                                <span className={s.number}>#{id}</span>
+                                <h3 className={s.name}>{name}</h3>
+                                <small className={s.type}>
+                                    Type: <span>{type}</span>
+                                </small>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="cardBack">
-                    <div className="wrap back">
-                        <img src="<-- Card Backed Picture -->" alt="Сard Backed" />
+                <div className={s.cardBack}>
+                    <div className={`${s.wrap} ${s.back}`}>
+                        <img src={cardBackSide} alt="Сard Backed" />
                     </div>
                 </div>
 
